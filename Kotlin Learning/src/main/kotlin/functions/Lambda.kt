54d256256ba2a0d14ebb4a5221314dfcc05f8653
Krishan <krishanyadav31081999@gmail.com>
2022-12-28 10:47:03 +0530

    Update 1.3

--- Kotlin Learning/src/main/kotlin/functions/Lambda.kt
+++ Kotlin Learning/src/main/kotlin/functions/Lambda.kt
@@ -1,21 +1,18 @@
-package Functions
-fun Tany() : String{
-    return "Hi"
-}
+package functions
+
 fun main() {
     val lambda = {x:Int , y : Int -> x+y}
 
     var multiline : ()-> (String) = {
         "This is Multiline lambda program"
     }
-
     val lambda2 : (Int)->Int = {x -> x *  x }
-
-
-    val sayHi : (String)-> Unit = { print("Varun hii") }
+    val sayHi : (String)-> Unit = { print("Varun Hii") }
 
 /// this is for Only Single argument
     var simplestform : (String)-> Unit = { print(it) }
     println(lambda(10,12))
-    print(multiline())
+    println(multiline())
+
+    println(simplestform("Here we know use of it Keyword"))
 }
--- Kotlin Learning/src/main/kotlin/functions/OperatorOverLoading.kt
+++ Kotlin Learning/src/main/kotlin/functions/OperatorOverLoading.kt
@@ -4,25 +4,27 @@ fun main() {
     val p = Hello(4)
     val q = Hello(8)
     val r = Hello(4)
-    if(p === r ){
+    if (p === r) {
         println("Matched !!")
-    }else{
+    } else {
         println("No ")
     }
     val x = p + q
-    val lef = arrayOf(1, 1, 1, 1,)
-    val righ = arrayOf(2, 2, 2, 2)
+    val left = arrayOf(1, 1, 1, 1)
+    val right = arrayOf(2, 2, 2, 2)
     val one = arrayOf(10, 11)
-    var mat = arrayOf(lef, righ, one)
+    var mat = arrayOf(left, right, one)
 
     println(mat[0][1])
 }
-class Hello(var x : Int){
+
+class Hello(var x: Int) {
     override fun equals(other: Any?): Boolean {
         return this.x.equals((other as Hello).x)
     }
 }
-operator fun Hello.plus( y : Hello) : Hello{
-     val p = this.x+ y.x + 2
-      return Hello(p)
+// extending plus operator for hello class objects
+operator fun Hello.plus(y: Hello): Hello {
+    val p = this.x + y.x + 2
+    return Hello(p)
 }
